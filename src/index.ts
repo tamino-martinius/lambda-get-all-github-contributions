aae27f4194ddec7de25e6ea800929eaaab7657ed
Tamino Martinius <dev@zaku.eu>
2018-06-26 14:58:03 +0200

    Change sample code

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,5 @@
-import vue from 'vue';
-import Hello from './components/Hello';
-import HelloDecorator from './components/HelloDecorator';
-import HelloVue from './components/Hello.vue';
-import HelloDecoratorVue from './components/HelloDecorator.vue';
-
-const v = new vue({
-  el: '#app',
-  template: `
-    <div>
-      Name: <input v-model="name" type="text">
-
-      <h1>Hello TS Component</h1>
-      <hello :name="name" :initialEnthusiasm="5" />
-      <h1>Hello TS Decorator Component</h1>
-      <hello-decorator :name="name" :initialEnthusiasm="5" />
-
-      <h1>Hello Vue Component</h1>
-      <hello-vue :name="name" :initialEnthusiasm="5" />
-      <h1>Hello Vue Decorator Component</h1>
-      <hello-decorator-vue :name="name" :initialEnthusiasm="5" />
-    </div>
-  `,
-  data: { name: 'World' },
-  components: {
-    Hello,
-    HelloDecorator,
-    HelloVue,
-    HelloDecoratorVue,
-  },
-});
+export default class Test {
+  constructor() {
+    console.log('hello world');
+  }
+}
