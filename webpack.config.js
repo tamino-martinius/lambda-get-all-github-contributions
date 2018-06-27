6f7eebe010d6920902513c0ddf7fb1cadf39ca6d
Tamino Martinius <dev@zaku.eu>
2018-06-27 22:14:52 +0200

    Change output filename

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   entry: './src/index.ts',
   output: {
     path: path.resolve(__dirname, './dist'),
-    filename: 'index.js',
+    filename: 'lambda.js',
   },
   module: {
     rules: [
