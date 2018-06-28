10d784b1b4dc2f54d9170fbc3071fc7c9e703b5a
Tamino Martinius <dev@zaku.eu>
2018-06-28 15:48:19 +0200

    Add script to update lambda function

---
+++ scripts/lambdaUpdate.ts
@@ -0,0 +1,23 @@
+import {
+  FUNCTION_NAME,
+  lambda,
+} from './env';
+import { action as packageAction } from './lambdaPackage';
+
+export const action = async () => {
+  const zip = await packageAction();
+  return await lambda.updateFunctionCode({
+    FunctionName: FUNCTION_NAME,
+    ZipFile: zip,
+    Publish: true,
+  }).promise();
+};
+export default action;
+
+if (!module.parent) {
+  // run action if script is called directly
+  (async () => {
+    const result = await action();
+    console.log(result);
+  })();
+}
