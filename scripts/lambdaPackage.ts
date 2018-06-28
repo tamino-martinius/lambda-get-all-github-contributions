2502d17239c9459f33a8148fa4db8f7b2cf715d1
Tamino Martinius <dev@zaku.eu>
2018-06-28 15:48:01 +0200

    Add script to package compiled code

---
+++ scripts/lambdaPackage.ts
@@ -0,0 +1,26 @@
+import {
+  action as buildAction,
+  TARGET_PATH,
+  TARGET_NAME,
+} from './build';
+import { readFileSync } from 'fs';
+
+import * as JSZip from 'jszip';
+
+export const action = async () => {
+  await buildAction(true);
+  const zip = new JSZip();
+  zip.file(TARGET_NAME, readFileSync(TARGET_PATH));
+  return await zip.generateAsync({
+    type: 'base64',
+  });
+};
+export default action;
+
+if (!module.parent) {
+  // run action if script is called directly
+  (async () => {
+    const result = await action();
+    console.log(result);
+  })();
+}
