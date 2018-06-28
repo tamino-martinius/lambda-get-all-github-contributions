b1ff77d67ecdcfd845adcd0f39a9832c9abd38c0
Tamino Martinius <dev@zaku.eu>
2018-06-28 15:47:45 +0200

    Remove implemented scripts

--- scripts/deploy_lambda_function.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-cd "$(dirname "$0")" || exit
-source ./env
-
-# Convenience script, just regrouping all lambda function scripts
-./package_lambda_function.sh
-./upload_lambda_function.sh
-./update_lambda_function.sh
