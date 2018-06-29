76f0d6f8182d5388426e19ff3e58c6e9aa111f9b
Tamino Martinius <dev@zaku.eu>
2018-06-29 09:49:09 +0200

    Update function with change set because of transforms

--- scripts/cloudFormationUpdateStack.ts
+++ scripts/cloudFormationUpdateStack.ts
@@ -1,6 +1,4 @@
 import {
-  FUNCTION_NAME,
-  BUCKET_NAME,
   STACK_NAME,
   cloudFormation,
   FINAL_STACK_TYPE,
@@ -9,11 +7,24 @@ import {
 
 export const action = async () => {
   const finalStack = generateStack(FINAL_STACK_TYPE);
-  return await cloudFormation.updateStack({
+  const changeSetName = `Update-${Date.now()}`;
+  await cloudFormation.createChangeSet({
     StackName: STACK_NAME,
     Capabilities: ['CAPABILITY_NAMED_IAM'],
+    ChangeSetName: changeSetName,
     Parameters: finalStack.parameters,
     TemplateBody: JSON.stringify(finalStack.template),
+  }).promise();
+  await cloudFormation.waitFor(
+    'changeSetCreateComplete',
+    {
+      StackName: STACK_NAME,
+      ChangeSetName: changeSetName,
+    },
+  ).promise();
+  await cloudFormation.executeChangeSet({
+    StackName: STACK_NAME,
+    ChangeSetName: changeSetName,
   }).promise();
 };
 export default action;
