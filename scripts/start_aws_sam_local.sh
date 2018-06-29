ffa55afdbdb4a0a9fece962bea41c3bd60fef57f
Tamino Martinius <dev@zaku.eu>
2018-06-29 09:47:02 +0200

    Remove bash scripts

--- scripts/start_aws_sam_local.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-cd "$(dirname "$0")" || exit
-source ./env
-
-AWS_REGION="$AWS_REGION" sam local start-api --template="$CLOUD_FORMATION_FILE" --host="$SAM_LOCAL_API_HOST" --port="$SAM_LOCAL_API_PORT"
--- scripts/upload_api_gateway_swagger.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-cd "$(dirname "$0")" || exit
-source ./env
-
-aws s3 cp "$API_GATEWAY_FILE" s3://"$BACKEND_S3_BUCKET" --region "$AWS_REGION"
