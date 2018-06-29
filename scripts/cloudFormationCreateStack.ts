10a85ff31685f3616152723b440129ac4488fd9a
Tamino Martinius <dev@zaku.eu>
2018-06-29 09:50:40 +0200

    Create new stack in two steps with change set

--- scripts/cloudFormationCreateStack.ts
+++ scripts/cloudFormationCreateStack.ts
@@ -1,30 +1,32 @@
 import {
-  FUNCTION_NAME,
-  BUCKET_NAME,
   STACK_NAME,
+  FINAL_STACK_TYPE,
+  TemplateType,
   cloudFormation,
-  template,
+  generateStack,
 } from './env';
+import { action as s3UploadAction } from './s3UploadApi';
+import { action as lambdaUploadAction } from './lambdaUpload';
+import { action as updateAction } from './cloudFormationUpdateStack';
 
 export const action = async () => {
-  return await cloudFormation.createStack({
+  const initialStack = generateStack(TemplateType.INITIAL);
+  await cloudFormation.createStack({
     StackName: STACK_NAME,
     Capabilities: ['CAPABILITY_NAMED_IAM'],
-    OnFailure: 'DELETE',
-    Parameters: [
-      {
-        ParameterKey: 'AwsServerlessExpressS3Bucket',
-        ParameterValue: BUCKET_NAME,
-        UsePreviousValue: false,
-      },
-      {
-        ParameterKey: 'LambdaFunctionName',
-        ParameterValue: FUNCTION_NAME,
-        UsePreviousValue: false,
-      },
-    ],
-    TemplateBody: JSON.stringify(template),
+    OnFailure: 'ROLLBACK',
+    Parameters: initialStack.parameters,
+    TemplateBody: JSON.stringify(initialStack.template),
   }).promise();
+  await cloudFormation.waitFor(
+    'stackCreateComplete',
+    { StackName: STACK_NAME },
+  ).promise();
+  if (FINAL_STACK_TYPE !== TemplateType.FUNCTION) {
+    await s3UploadAction();
+  }
+  await lambdaUploadAction();
+  return await updateAction();
 };
 export default action;
 
