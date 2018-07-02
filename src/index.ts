95effa2d8f89a9a6ddf01a6c4b9025c4a3259b40
Tamino Martinius <dev@zaku.eu>
2018-07-02 20:00:57 +0200

    Add github graphql init

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,22 @@
-class Test {
+import { GitHub } from 'github-graphql-api';
+import {
+  RepositoryPage,
+} from './types';
+const apiToken = process.env.GITHUB_TOKEN;
+
+if (!apiToken) throw 'please define "GITHUB_TOKEN" env variable';
+
+const github = new GitHub({
+  token: apiToken,
+});
+
+class Cron {
   constructor() {
-    console.log('hello world');
   }
 }
 
-console.log('test');
+console.log(apiToken);
 
-export default async (event: any) => {
-  return new Test();
+export default async (event: any, context: AWSLambda.Context) => {
+  const cron = new Cron();
 };
