87cdf456b2700f6f98e263dbe341eb47c6f4664c
Tamino Martinius <dev@zaku.eu>
2018-07-02 20:04:52 +0200

    Move cron to seperate file

---
+++ src/Cron.ts
@@ -0,0 +1,61 @@
+import { GitHub } from 'github-graphql-api';
+import {
+  RepositoryPage,
+} from './types';
+const apiToken = process.env.GITHUB_TOKEN;
+
+if (!apiToken) throw 'please define "GITHUB_TOKEN" env variable';
+
+const github = new GitHub({
+  token: apiToken,
+});
+
+export class Cron {
+  constructor() {
+    console.log(apiToken);
+  }
+
+  async getRepositoryNames(): Promise<string[]> {
+    let hasNextPage = true;
+    let endCursor: string | undefined = undefined;
+    const repositoryNames: string[] = [];
+    while (hasNextPage) {
+      const repositoryPage: RepositoryPage = await this.getRepositoryPage(endCursor);
+      const pageInfo = repositoryPage.viewer.repositories.pageInfo;
+      hasNextPage = pageInfo.hasNextPage;
+      endCursor = pageInfo.endCursor;
+      const nodes = repositoryPage.viewer.repositories.nodes;
+      repositoryNames.push(...nodes.map(node => node.nameWithOwner));
+    }
+    return repositoryNames;
+  }
+
+  static pageQuery(startCursor?: string): string {
+    let query = 'first: 100';
+    if (startCursor) {
+      query += `, after: "${startCursor}"`;
+    }
+    return query;
+  }
+
+  async getRepositoryPage(startCursor?: string): Promise<RepositoryPage> {
+    return await github.query(`
+      query {
+        viewer{
+          repositories(${Cron.pageQuery(startCursor)}) {
+            totalCount
+            pageInfo {
+              hasNextPage
+              endCursor
+            }
+            nodes {
+              nameWithOwner
+            }
+          }
+        }
+      }
+    `);
+  }
+}
+
+export default Cron;
