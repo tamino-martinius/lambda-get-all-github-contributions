275aa21d700f24807d188b9856c4a1c2f198ba0c
Tamino Martinius <dev@zaku.eu>
2018-07-03 19:12:14 +0200

    Create DB by factory

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,6 @@ import DB from './DB';
 export default async (event: any, context: AWSLambda.Context) => {
   // const cron = await Cron.create();
   // console.log(JSON.stringify(cron.repositories));
-  console.log(new DB());
-
+  const db = await DB.create();
+  console.log(db);
 };
