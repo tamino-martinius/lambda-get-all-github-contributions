cea6e3a5f27ed6fc2db97ae029147b4b2f344022
Tamino Martinius <dev@zaku.eu>
2018-07-03 19:11:29 +0200

    Add scaffold for db init

--- src/DB.ts
+++ src/DB.ts
@@ -1,7 +1,6 @@
 import { DynamoDB } from 'aws-sdk';
 const AWS_REGION = process.env.AWS_REGION || 'eu-central-1';
 const ENDPOINT = process.env.AWS_SAM_LOCAL ? 'http://docker.for.mac.localhost:8000' : undefined;
-console.log(process.env);
 
 const dynamoDB = new DynamoDB({
   region: AWS_REGION,
@@ -14,7 +13,19 @@ const dynamoDBClient = new DynamoDB.DocumentClient({
 });
 
 export class DB {
+  constructor() {
 
+  }
+
+  static async create(): Promise<DB> {
+    const db = new DB();
+    await db.init();
+    return db;
+  }
+
+  async init() {
+
+  }
 }
 
 export default DB;
