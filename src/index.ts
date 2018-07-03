f469dce80ee19734c7466b38532e1f6a2486c1c6
Tamino Martinius <dev@zaku.eu>
2018-07-03 19:08:55 +0200

    Change logging to DB

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import Cron from './Cron';
+import DB from './DB';
 
 export default async (event: any, context: AWSLambda.Context) => {
-  const cron = await Cron.create();
-  console.log(JSON.stringify(cron.repositories));
+  // const cron = await Cron.create();
+  // console.log(JSON.stringify(cron.repositories));
+  console.log(new DB());
+
 };
