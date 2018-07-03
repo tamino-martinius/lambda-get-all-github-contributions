0fe441e8bd589dd5a5568892f037320791e60b15
Tamino Martinius <dev@zaku.eu>
2018-07-03 19:09:11 +0200

    Import and initialize DynamoDB

--- src/DB.ts
+++ src/DB.ts
@@ -1,3 +1,18 @@
+import { DynamoDB } from 'aws-sdk';
+const AWS_REGION = process.env.AWS_REGION || 'eu-central-1';
+const ENDPOINT = process.env.AWS_SAM_LOCAL ? 'http://docker.for.mac.localhost:8000' : undefined;
+console.log(process.env);
+
+const dynamoDB = new DynamoDB({
+  region: AWS_REGION,
+  endpoint: ENDPOINT,
+});
+
+const dynamoDBClient = new DynamoDB.DocumentClient({
+  region: AWS_REGION,
+  endpoint: ENDPOINT,
+});
+
 export class DB {
 
 }
