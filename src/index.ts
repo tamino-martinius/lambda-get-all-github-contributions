f68b49f5ef9aade3c41494b50c39867ff4f76f35
Tamino Martinius <dev@zaku.eu>
2018-07-06 13:32:19 +0200

    Fetch data for user

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@ import Cron from './Cron';
 import DB from './DB';
 
 export default async (event: any, context: AWSLambda.Context) => {
-  // const cron = await Cron.create();
-  // console.log(JSON.stringify(cron.repositories));
-  const db = await DB.create();
-  console.log(db);
+  const cron = await Cron.create('tamino-martinius');
+  console.log(JSON.stringify(cron.repositories));
+  // const db = await DB.create();
+  // console.log(db);
 };
