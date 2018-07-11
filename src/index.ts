2e606736cf50d2b99ce38a61f9a1f89f1ec42659
Tamino Martinius <dev@zaku.eu>
2018-07-11 08:39:19 +0200

    Changed logs to hasChanged

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,8 @@ import Stats from './Stats';
 
 export default async (event: any, context: AWSLambda.Context) => {
   const crawler = await Crawler.create('tamino-martinius');
+  console.log('crawler changed?', crawler.hasChanged);
   const stats = await Stats.create(crawler);
-  // console.log(stats);
-  return crawler.crawlType;
+  console.log('stats changed?', stats.hasChanged);
+  return stats.hasChanged;
 };
