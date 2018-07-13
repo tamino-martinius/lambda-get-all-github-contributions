f4f7891ee707b56950d7b0be6dcd222f42298f38
Tamino Martinius <dev@zaku.eu>
2018-07-13 08:15:11 +0200

    Get week number from date

--- src/Stats.ts
+++ src/Stats.ts
@@ -73,6 +73,14 @@ export class Stats implements StatsPosition {
     stats.weekDays = {};
   }
 
+  static getWeekFromDate(date: Date) {
+    const d = new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()));
+    const dayNum = d.getUTCDay() || 7;
+    d.setUTCDate(d.getUTCDate() + 4 - dayNum);
+    const yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1));
+    return Math.ceil((((d.getTime() - yearStart.getTime()) / 86400000) + 1) / 7);
+  }
+
   static async create(crawler: Crawler): Promise<Stats> {
     const storage = await Storage.create();
     const stats = new Stats(crawler, storage);
