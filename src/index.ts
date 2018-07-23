94c0d494bcdd5db4e54d4f2108aafe66c6dccba3
Tamino Martinius <dev@zaku.eu>
2018-07-23 22:45:35 +0200

    Catch all errors

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,13 @@ import Crawler from './Crawler';
 import Stats from './Stats';
 
 export default async (event: any, context: AWSLambda.Context) => {
-  const crawler = await Crawler.create('tamino-martinius');
-  console.log('crawler changed?', crawler.hasChanged);
-  const stats = await Stats.create(crawler);
-  console.log('stats changed?', stats.hasChanged);
-  return stats.hasChanged;
+  try {
+    const crawler = await Crawler.create('tamino-martinius');
+    console.log('crawler changed?', crawler.hasChanged);
+    const stats = await Stats.create(crawler);
+    console.log('stats changed?', Object.keys(stats.stats.repositories));
+  } catch (error) {
+    console.log(error);
+  }
+  return true;
 };
