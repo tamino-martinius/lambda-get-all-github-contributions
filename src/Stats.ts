29853e2aa13e39f455f1a1eb7565ec380252cbd3
Tamino Martinius <dev@zaku.eu>
2018-07-23 22:45:28 +0200

    Refactor stats generation

--- src/Stats.ts
+++ src/Stats.ts
@@ -2,44 +2,49 @@ import {
   Commit,
   Dict,
   Repository,
-  StatsKey,
-  StatsPosition,
-  Totals,
-  TimeStats,
+  Counts,
+  StatsData,
   WeekDayStats,
+  CommitStats,
+  RepositoryStats,
 } from './types';
 import Storage from './Storage';
 import Crawler from './Crawler';
 
-export class Stats implements StatsPosition {
+export class Stats {
   crawler: Crawler;
   storage: Storage;
-  stats: TimeStats = Stats.emptyStats;
-  repositoryMapping: Dict<string> = {};
-  repositoryStats: Dict<TimeStats> = {};
-  nextPrivateId: number = 1;
-  processedCommits: Dict<Commit> = {};
-  lastData?: string;
-  hasChanged: boolean = false;
+  stats: StatsData = {
+    total: {
+      closed: Stats.emptyCounts,
+      open: Stats.emptyCounts,
+      sum: Stats.emptyCounts,
+    },
+    languages: {},
+    weekDays: {
+      closed: {},
+      open: {},
+      sum: {},
+    },
+    dates: {
+      closed: {},
+      open: {},
+      sum: {},
+    },
+    repositories: {},
+  };
+  nextPrivateId: number = 0;
 
   constructor(crawler: Crawler, storage: Storage) {
     this.crawler = crawler;
     this.storage = storage;
   }
 
-  get positionId() {
-    return `${ this.crawler.userId }-stats`;
-  }
-
   get statsId() {
     return this.crawler.userLogin;
   }
 
-  get repositoryId() {
-    return `${this.crawler.userLogin}-repositories`;
-  }
-
-  static get emptyTotals(): Totals {
+  static get emptyCounts(): Counts {
     return {
       additions: 0,
       deletions: 0,
@@ -48,25 +53,17 @@ export class Stats implements StatsPosition {
     };
   }
 
-  static get emptyWeekDayStats(): WeekDayStats {
+  static get emptyWeekDay(): WeekDayStats {
     return {
-      additions: 0,
-      deletions: 0,
-      changedFiles: 0,
-      commitCount: 0,
+      ...this.emptyCounts,
       hours: {},
     };
   }
 
-  static get emptyStats(): TimeStats {
+  static get emptyRepository(): RepositoryStats {
     return {
-      quarterly: {},
-      hourly: {},
-      daily: {},
-      weekly: {},
-      monthly: {},
-      yearly: {},
-      weekDays: {},
+      ...this.emptyCounts,
+      years: {},
     };
   }
 
@@ -81,135 +78,115 @@ export class Stats implements StatsPosition {
   static async create(crawler: Crawler): Promise<Stats> {
     const storage = await Storage.create();
     const stats = new Stats(crawler, storage);
-    await stats.restore();
     await stats.init();
     return stats;
   }
 
-  static addCommitToTotals(commit: Commit, total: Totals) {
+  static addCommitToCounts(commit: Commit, total: Counts) {
     total.additions += commit.additions;
     total.deletions += commit.deletions;
     total.changedFiles += commit.changedFiles;
     total.commitCount += 1;
   }
 
-  initRepositories() {
-    for (const repoKey in this.crawler.repositories) {
-      console.log(`Init repo stats for ${repoKey}`);
-      if (!this.repositoryMapping[repoKey]) {
-        const repo = this.crawler.repositories[repoKey];
-        if (repo.isPrivate) {
-          const privateKey = `${ repo.owner }/private#${ this.nextPrivateId }`;
-          this.nextPrivateId += 1;
-          this.repositoryMapping[repoKey] = privateKey;
-          this.repositoryMapping[privateKey] = repoKey;
-          this.repositoryStats[privateKey] =
-            this.repositoryStats[privateKey] || Stats.emptyStats;
-        } else {
-          this.repositoryMapping[repoKey] = repoKey;
-          this.repositoryStats[repoKey] =
-            this.repositoryStats[repoKey] || Stats.emptyStats;
-        }
-      }
-    }
+  static addCounts(total: Counts, commit: CommitStats) {
+    total.additions += commit.additions;
+    total.deletions += commit.deletions;
+    total.changedFiles += commit.changedFiles;
+    total.commitCount += 1;
   }
 
-  initTimeline(repo: Repository, commit: Commit, key: string, statsKey: StatsKey) {
-    const repoStats = this.repositoryStats[this.repositoryMapping[repo.key]];
-    this.stats[statsKey][key] = this.stats[statsKey][key] || Stats.emptyTotals;
-    repoStats[statsKey][key] = repoStats[statsKey][key] || Stats.emptyTotals;
-    const totals = [this.stats[statsKey][key], repoStats[statsKey][key]];
-    for (const total of totals) {
-      Stats.addCommitToTotals(commit, total);
-    }
+  static getCommitStats(repo: Repository, commit: Commit): CommitStats {
+    const date = new Date(commit.committedDate);
+    return {
+      additions: commit.additions,
+      deletions: commit.deletions,
+      changedFiles: commit.changedFiles,
+      isPrivate: repo.isPrivate,
+      dateStr: `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`,
+      monthStr: `${date.getFullYear()}-${date.getMonth()}`,
+      weekStr: `${date.getFullYear()}-${Stats.getWeekFromDate(date)}`,
+      yearStr: `${date.getFullYear()}`,
+      weekDayStr: `${date.getDay()}`,
+      hourStr: `${date.getHours()}`,
+      quarterStr: `${date.getHours()}-${~~(date.getMinutes() / 15)}`,
+    };
   }
 
-  initWeekDay(repo: Repository, commit: Commit, weekDayStr: string, hourStr: string) {
-    const repoStats = this.repositoryStats[this.repositoryMapping[repo.key]];
-    this.stats.weekDays[weekDayStr] = this.stats.weekDays[weekDayStr] || Stats.emptyWeekDayStats;
-    repoStats.weekDays[weekDayStr] = repoStats.weekDays[weekDayStr] || Stats.emptyWeekDayStats;
-    const totals = [this.stats.weekDays[weekDayStr], repoStats.weekDays[weekDayStr]];
-    for (const total of totals) {
-      total.hours[hourStr] = total.hours[hourStr] || Stats.emptyTotals;
-      Stats.addCommitToTotals(commit, total);
-      Stats.addCommitToTotals(commit, total.hours[hourStr]);
-    }
+  addRepo(name: string, commit: CommitStats) {
+    this.stats.repositories[name] = this.stats.repositories[name] || Stats.emptyRepository;
+    const repo = this.stats.repositories[name];
+    Stats.addCounts(repo, commit);
+
+    const year = commit.yearStr;
+    repo.years[year] = repo.years[year] || Stats.emptyCounts;
+    Stats.addCounts(repo.years[year], commit);
   }
 
-  initStats() {
-    for (const repoKey in this.crawler.repositories) {
-      console.log(`Generate repo stats for ${repoKey}`);
-      const repo = this.crawler.repositories[repoKey];
-      for (const commitKey of repo.ownCommits) {
-        if (!this.processedCommits[commitKey]) {
-          const commit = repo.commits[commitKey];
-          const date = new Date(commit.committedDate);
-          const dateStr = `${ date.getFullYear() }-${ date.getMonth() }-${ date.getDate() }`;
-          const monthStr = `${ date.getFullYear() }-${ date.getMonth() }`;
-          const weekStr = `${ date.getFullYear() }-${ Stats.getWeekFromDate(date) }`;
-          const yearStr = `${ date.getFullYear() }`;
-          const weekDayStr = `${ date.getDay() }`;
-          const hourStr = `${ date.getHours() }`;
-          const quarterStr = `${ date.getHours() }-${ ~~(date.getMinutes() / 15) }`;
-          this.initTimeline(repo, commit, quarterStr, 'quarterly');
-          this.initTimeline(repo, commit, hourStr, 'hourly');
-          this.initTimeline(repo, commit, dateStr, 'daily');
-          this.initTimeline(repo, commit, weekStr, 'weekly');
-          this.initTimeline(repo, commit, monthStr, 'monthly');
-          this.initTimeline(repo, commit, yearStr, 'yearly');
-          this.initWeekDay(repo, commit, weekDayStr, hourStr);
-          this.processedCommits[commitKey] = commit;
-        }
-      }
-    }
+  addLanguage(language: string, commit: CommitStats) {
+    this.stats.languages[language] = this.stats.languages[language] || Stats.emptyCounts;
+    Stats.addCounts(this.stats.languages[language], commit);
+  }
+
+  addCommitToCounts(commit: CommitStats) {
+    const { closed, open, sum } = this.stats.total;
+    Stats.addCounts(commit.isPrivate ? closed : open, commit);
+    Stats.addCounts(sum, commit);
+  }
+
+  addCommitToWeekDays(commit: CommitStats) {
+    const { closed, open, sum } = this.stats.weekDays;
+    const wd = commit.weekDayStr;
+    closed[wd] = closed[wd] || Stats.emptyWeekDay;
+    open[wd] = open[wd] || Stats.emptyWeekDay;
+    sum[wd] = sum[wd] || Stats.emptyWeekDay;
+    Stats.addCounts(commit.isPrivate ? closed[wd] : open[wd], commit);
+    Stats.addCounts(sum[wd], commit);
+
+    const hour = commit.hourStr;
+    closed[wd].hours[hour] = closed[wd].hours[hour] || Stats.emptyCounts;
+    open[wd].hours[hour] = open[wd].hours[hour] || Stats.emptyCounts;
+    sum[wd].hours[hour] = sum[wd].hours[hour] || Stats.emptyCounts;
+    Stats.addCounts(commit.isPrivate ? closed[wd].hours[hour] : open[wd].hours[hour], commit);
+    Stats.addCounts(sum[wd].hours[hour], commit);
+  }
+
+  addCommitToDates(commit: CommitStats) {
+    const { closed, open, sum } = this.stats.dates;
+    const date = commit.dateStr;
+    closed[date] = closed[date] || Stats.emptyCounts;
+    open[date] = open[date] || Stats.emptyCounts;
+    sum[date] = sum[date] || Stats.emptyCounts;
+    Stats.addCounts(commit.isPrivate ? closed[date] : open[date], commit);
+    Stats.addCounts(sum[date], commit);
+  }
+
+  addCommit(commit: CommitStats) {
+    this.addCommitToCounts(commit);
+    this.addCommitToWeekDays(commit);
+    this.addCommitToDates(commit);
   }
 
   async init() {
-    this.initRepositories();
-    this.initStats();
+    for (const repoKey in this.crawler.repositories) {
+      console.log(`Init stats for ${repoKey}`);
+      const repo = this.crawler.repositories[repoKey];
+      const name = repo.isPrivate ? `${repo.owner}/private#${this.nextPrivateId += 1}` : repoKey;
+      for (const commitKey of repo.ownCommits) {
+        const commit = Stats.getCommitStats(repo, repo.commits[commitKey]);
+        this.addCommit(commit);
+        this.addRepo(name, commit);
+        for (const language of repo.languages) {
+          this.addLanguage(language, commit);
+        }
+      }
+    }
     await this.save();
-  }
-
-  get position(): StatsPosition {
-    return {
-      stats: this.stats,
-      repositoryMapping: this.repositoryMapping,
-      repositoryStats: this.repositoryStats,
-      nextPrivateId: this.nextPrivateId,
-      processedCommits: this.processedCommits,
-    };
   }
 
   async save() {
     console.log(`Save stats`);
-    const dataStr = JSON.stringify(this.position);
-    const hasChanged = this.lastData !== dataStr;
-    if (hasChanged) {
-      console.log(`Write ${dataStr.length} chars`);
-      this.lastData = dataStr;
-      await this.storage.writeItem(this.positionId, dataStr);
-      await this.storage.writeItem(this.statsId, JSON.stringify(this.stats));
-      await this.storage.writeItem(this.repositoryId, JSON.stringify(this.repositoryStats));
-    } else {
-      console.log('skipped writing - no changes detected');
-    }
-    this.hasChanged = this.hasChanged || hasChanged;
-    return hasChanged;
-  }
-
-  async restore() {
-    console.log(`Restore stats`);
-    const dataStr = await this.storage.readItem(this.positionId);
-    if (dataStr) {
-      console.log(`Read ${dataStr.length} chars`);
-      const data: StatsPosition = JSON.parse(dataStr);
-      this.lastData = dataStr;
-      this.stats = data.stats;
-      this.repositoryMapping = data.repositoryMapping;
-      this.repositoryStats = data.repositoryStats;
-      this.nextPrivateId = data.nextPrivateId;
-      this.processedCommits = data.processedCommits;
-    }
+    return await this.storage.writeItem(this.statsId, JSON.stringify(this.stats));
   }
 }
 
